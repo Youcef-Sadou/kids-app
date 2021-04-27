38f581ba140c1bfbd2325522167c0f9243e0b260
sadou <youcefsadou2@gmail.com>
2021-04-27 10:26:35 +0100

    Added the task activity where we will add our tasks, and created the T class that describes the attributes of any given task (time of the task , icon that represents the task, and description of the task)

--- app/src/main/java/com/example/kidsapp/MainActivity.java
+++ app/src/main/java/com/example/kidsapp/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.kidsapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> tasklist_dates  = new ArrayList<String>();
+   // LinearLayout layoutList = findViewById(R.id.layout_list);
+    int cpt = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LinearLayout layoutList = findViewById(R.id.layout_list);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -20,10 +28,6 @@ public class MainActivity extends AppCompatActivity {
             //startActivity(intent);
             startActivityForResult(intent, 3);
         });
-
-
-
-
     }
 
     // This method is called when the second activity finishes
@@ -38,18 +42,40 @@ public class MainActivity extends AppCompatActivity {
                 // Get String data from Intent
                 String returnString = data.getStringExtra("keyName");
 
-                // Set text view with string
-               /* TextView textView = findViewById(R.id.textView);
 
-                textView.setText(returnString);
-                */
                 TextView textView = findViewById(R.id.textView);
-                textView.setText(returnString);
+                tasklist_dates.add(returnString);
+                cpt++;
+                textView.setText("");
+
+
+
+
+                final View tasklistView = getLayoutInflater().inflate(R.layout.row_add_tasklist,null,false);
+                //this creates a new view
+
+
+                //this locates the list
+                LinearLayout layoutList = findViewById(R.id.layout_list);
+
+
+                layoutList.addView(tasklistView);
+
+
+
+                for(int i = 0; i <(layoutList.getChildCount()) ;i++){
+                    TextView tv = (TextView) layoutList.getChildAt(i);
+
+                    tv.setText(tasklist_dates.get(i));
+
+                    // Making it basically a button to the tasks activity
+                    tv.setOnClickListener((v) -> {
+                        Intent intent = new Intent(this, Task.class);
+                        startActivity(intent);
+                    });
+                }
             }
         }
     }
-
-
-
 
 }   
